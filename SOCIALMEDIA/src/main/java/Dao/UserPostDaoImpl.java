0b3b5961c3a6a8fac97d5b48e3737f79b12ebb52
quythuong <quythuong001@gmail.com>
2023-11-19 13:25:15 +0700

    add getAllPostsOfUser function

--- SOCIALMEDIA/src/main/java/Dao/UserPostDaoImpl.java
+++ SOCIALMEDIA/src/main/java/Dao/UserPostDaoImpl.java
@@ -11,7 +11,8 @@ import JpaConfig.JPAConfig;
 public class UserPostDaoImpl implements IUserPostDao {
 
 	public static void main(String[] args) {
-		System.out.println(new UserPostDaoImpl().paginationPage(12,6));
+		//System.out.println(new UserPostDaoImpl().paginationPage(0,2));
+		System.out.println(new UserPostDaoImpl().getAllPostsOfUser(1));
 	}
 	@Override
 	public List<UserPost> paginationPage(int index, int numberOfPage) {
@@ -19,6 +20,13 @@ public class UserPostDaoImpl implements IUserPostDao {
 		TypedQuery<UserPost> query = entityManager.createNamedQuery("UserPost.findAll", UserPost.class);
 		query.setFirstResult(index);
 		query.setMaxResults(numberOfPage);
+		return query.getResultList();
+	}
+	public List<UserPost> getAllPostsOfUser(int userId) {
+		EntityManager entityManager = JPAConfig.getEntityManager();
+		String jpql = "select uP from UserPost uP where uP.user.userID = " + String.valueOf(userId);
+		TypedQuery<UserPost> query = entityManager.createQuery(jpql, UserPost.class);
+		//query.setParameter("userId", "%" + String.valueOf(userId) + "%");
 		return query.getResultList();
 	}
 
