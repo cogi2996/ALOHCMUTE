70d08672a93a9b14b016ed6c9036146cdb25a2e0
ChiuKyubey <ndhuong1616@gmail.com>
2023-12-07 12:32:59 +0700

    .giao dien hiring.

--- SOCIALMEDIA/src/main/java/Entity/Chat.java
+++ SOCIALMEDIA/src/main/java/Entity/Chat.java
@@ -1,49 +1,70 @@
-//package Entity;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.NamedQuery;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "Chat")
-//@NamedQuery(name = "Chat.findAll", query = "select p from Chat p")
-//public class Chat {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int chatID;
-////	private int boxChatID;
-//
-//	// 1-nhiều giữa chat với chatbox
-//	@ManyToOne
-//	@JoinColumn(name = "boxChatID")
-//	private BoxChat boxChat;
-//
-//
-//	public Chat() {
-//		super();
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public int getChatID() {
-//		return chatID;
-//	}
-//
-//	public void setChatID(int chatID) {
-//		this.chatID = chatID;
-//	}
-//
-//
-//	public BoxChat getBoxChat() {
-//		return boxChat;
-//	}
-//
-//	public void setBoxChat(BoxChat boxChat) {
-//		this.boxChat = boxChat;
-//	}
-//
-//}
+package Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Chat")
+@NamedQuery(name = "Chat.findAll", query = "select c from Chat c")
+@IdClass(ClassId.class)
+public class Chat implements Serializable {
+	@Id
+    private String sourceID;
+
+    @Id
+    private String targetID;
+	@ManyToOne
+	@JoinColumn( name= "userID")
+	private  User user;
+	
+	@Override
+	public String toString() {
+		return "Chat{" +
+                ", sourceID='" + sourceID + '\'' +
+                ", targetID='" + targetID + '\'' +
+                '}';
+	}
+	
+	public Chat(String sourceID, String targetID, User user) {
+		super();
+		this.sourceID = sourceID;
+		this.targetID = targetID;
+		this.user = user;
+	}
+	public Chat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getSourceID() {
+		return sourceID;
+	}
+	
+	public void setSourceID(String sourceID) {
+		this.sourceID = sourceID;
+	}
+	
+	public String getTargetID() {
+		return targetID;
+	}
+	
+	public void setTargetID(String targetID) {
+		this.targetID = targetID;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user)
+	{
+		this.user = user;
+	}
+	
+}
