1e70ff57d327870fea88ddc25eeda6955cf4ce19
Van Tin <vantin22112003@gmail.com>
2023-12-07 13:10:36 +0700

    Merge branch 'main' of https://github.com/cogi2996/ALOHCMUTE.git into tin-api

--- SOCIALMEDIA/src/main/java/Controller/user/ProfileController.java
+++ SOCIALMEDIA/src/main/java/Controller/user/ProfileController.java
@@ -7,10 +7,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = "/profile")
+
+import Entity.User;
+import Services.IUserService;
+import Services.UserServiceImpl;
+@WebServlet(urlPatterns = {"/profile","/editProfile"})
 public class ProfileController extends HttpServlet{
+	IUserService userService = new UserServiceImpl();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/views/user/profile.jsp").forward(req, resp);
+		String url = req.getRequestURL().toString();
+		if(url.contains("profile")) {
+			String uid = req.getParameter("userID");
+			User user =  userService.findUser(uid);
+			req.setAttribute("user", user );
+			req.getRequestDispatcher("/views/user/profile.jsp").forward(req, resp);
+		}
+		else if(url.contains("editProfile")){
+//			String uid = req.getParameter("userID");
+//			User user =  userService.findUser(uid);
+//			req.setAttribute("user", user );
+			req.getRequestDispatcher("/views/user/editProfile.jsp").forward(req, resp);
+			
+		}
+		
+		
 	}
+	
 }
